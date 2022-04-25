519a10ddbb9974a3181bfca9d98e0fe048b1bc17
Aleksei <Mr.Puzako@gmail.com>
2022-04-25 21:05:05 +0300

    fix: add script

---
+++ js_modules/menuSwitching.js
@@ -0,0 +1,34 @@
+const menuBody = document.querySelector(".menu__body");
+
+const iconBurger = document.querySelector(".menu__icon");
+
+const blackoutBlock = document.querySelector(".blackout-screan");
+
+function closeMenu(arr) {
+	arr.forEach(el => el.classList.remove("active"));
+}
+
+function openMenu(...arr) {
+	arr.forEach(el => el.classList.toggle("active"));
+	document.body.classList.toggle("lock");
+}
+
+export const switchMenu = () => {
+	iconBurger.addEventListener("click", () => {
+		openMenu.apply(this, [menuBody, iconBurger, blackoutBlock]);
+	});
+
+	menuBody.addEventListener("click", (e) => {
+		if (e.target.classList.contains("menu__link")) {
+			closeMenu([menuBody, iconBurger, blackoutBlock]);
+			document.body.classList.remove("lock");
+		}
+	});
+
+	document.addEventListener("mousedown", (e) => {
+		if (!e.target.closest(".menu") && !e.target.closest(".info-popap__content")) {
+			closeMenu([menuBody, iconBurger, blackoutBlock]);
+			document.body.classList.remove("lock");
+		}
+	});
+}
---
+++ pages/main/script.js
@@ -0,0 +1,13 @@
+import { switchMenu } from "../../js_modules/menuSwitching.js";
+import { changeViewHeader } from "../../js_modules/changeViewHeader.js";
+import { switchPopap } from "../../js_modules/switchPopap.js";
+import { scrollToblock, scrollDecorationLink } from "../../js_modules/scrollToBlock.js";
+
+changeViewHeader();
+scrollToblock();
+scrollDecorationLink();
+switchMenu();
+switchPopap();
+
+
+
